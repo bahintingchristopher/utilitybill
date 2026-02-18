6f5674cd96eed38e6d8d8cf8d92f4b0a64ec36b3
Christopher Bahinting <bahintingchristopher@gmail.com>
2026-02-18 22:12:21 +0800

    unpdate wsgi code

--- waterbill/wsgi.py
+++ waterbill/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for waterbill project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# The path to your project
+path = '/home/utilitybill'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'waterbill.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'waterbill.settings')
-
 application = get_wsgi_application()
